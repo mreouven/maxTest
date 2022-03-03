489a362307c9bd57d7a181f5a4fe49386af6c65b
Reouven Mimoun <37777368+mreouven@users.noreply.github.com>
2022-03-03 20:51:02 +0200

    Initial Commit

---
+++ maxTest/maxTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  maxTestApp.swift
+//  maxTest
+//
+//  Created by Reouven Mimoun on 03/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct maxTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
